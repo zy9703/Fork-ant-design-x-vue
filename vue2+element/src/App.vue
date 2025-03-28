4d4bd3591fa91018102650b83ece27de1d71c7ee
ashley <52343662+zy9703@users.noreply.github.com>
2025-03-28 18:00:02 +0800

    引入：elementUI

--- vue2+element/src/App.vue
+++ vue2+element/src/App.vue
@@ -1,19 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Ant Design X Vue（Vue2 + Element UI版本）</h1>
+    <el-tabs v-model="activeTab">
+      <el-tab-pane label="气泡组件 Bubble" name="bubble">
+        <bubble-example />
+      </el-tab-pane>
+      <!-- 后续可以在这里添加更多组件的示例 -->
+    </el-tabs>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BubbleExample from './views/BubbleExample.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BubbleExample
+  },
+  data() {
+    return {
+      activeTab: 'bubble'
+    };
   }
-}
+};
 </script>
 
 <style>
@@ -21,8 +31,18 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  max-width: 1200px;
+  margin: 20px auto;
+  padding: 0 20px;
+}
+
+h1 {
+  text-align: center;
+  margin-bottom: 30px;
+}
+
+.el-tabs {
+  margin-bottom: 40px;
 }
 </style>
--- vue2+element/src/main.js
+++ vue2+element/src/main.js
@@ -1,5 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import Components from './components'
+
+// 注册Element UI
+Vue.use(ElementUI)
+
+// 使用自定义组件库
+Vue.use(Components)
 
 Vue.config.productionTip = false
 
